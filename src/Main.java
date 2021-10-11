3467da5fc74fdf975e96fc0cb5adf5e092993eed
TonyMorrowz <89682214+TonyMorrowz@users.noreply.github.com>
2021-10-11 14:04:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,14 @@ public class Main {
     public static void main(String[] args) {
         String[] data = new String[] {"3","2hgg","1","4"};
         int[] data2 = new int[4];
+        try{
         for (int i = 0; i < data.length; i++) {
-            data2[i] = Integer.parseInt(data[i]);
+            data2[i] = Integer.parseInt(data[i]);}
         }
-        for (int i = 0; i < data2.length; i++) {
+        catch (NumberFormatException e){
+
+        }
+                for (int i = 0; i < data2.length; i++) {
             System.out.println(data2[i]);
 
         }
