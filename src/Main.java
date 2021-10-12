9e14163a5295e86ba17a4b30eb1c41739fd9f9da
Venom <30825272+AntonZarytski@users.noreply.github.com>
2021-10-12 09:17:59 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@
  * Тоесть вопрос к тебе что сделать с этим кодом что бы на выходе получить 3 2 1 4
  */
 public class Main {
+    private List<String> list;
     public static void main(String[] args) {
         String[] data = new String[] {"3","2hgg","1","4"};
         int[] data2 = new int[4];
@@ -14,14 +15,33 @@ public class Main {
             data2[i] = Integer.parseInt(data[i]);}
         }
         catch (NumberFormatException e){
-
+// сюда ты добавляешь логику обработки исключений(никакой ключевой логики для работы аппы в catch добавлять не рекомендуется),
+// а так же можешь отобразить исключение
+            // e.printStackTrace() или Log.e(TAG, e)
+//если в блоке catch возникнет иисключение, то аппа упадет
+        } finnaly {
+            // посмотри что такое finnaly
         }
-                for (int i = 0; i < data2.length; i++) {
+        for (int i = 0; i < data2.length; i++) {
             System.out.println(data2[i]);
 
         }
-
+        // list = new ArrayList();
+        getListSize(size) == -1 ? 
+        System.out.println("Exception was triggered"):
+        System.out.println("Exception wasnt triggered, listSize is " + getListSize(list)):
     }
-
+    
+    // наример метод с exception, когда тебе надо вернуть что-то, где может возникнуть исключение, вообще варианвто может быть дофига
+    int getListSize(List<T> list){
+        int answer = 0;
+        try{
+            answer = list.size();
+        }catch (NullPointerException e){
+            answer = -1;
+            Log.e(TAG, e);
+        }
+        return answer
+    }                 
 }
 
